7d083aecb86afb993585e940adbb7049e63cc7cf
greg78ramos <gregoriomoralesramos17@gmail.com>
2026-04-09 13:41:23 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+   int num, cont = 0;
+   printf("\a----- Tabla de multiplicar -----\n");
+   printf("Ingrese un número: \n");
+   scanf("%d", &num);
+   printf("La tabla de multiplicar del %d es:\n", num);
+   while (++cont <= 10)
+   printf("%d x %d = %d\n", num, cont, num*cont);
+   return 0;
+}
+
+FUNCIÒN PRINCIPAL VOID
+
+#include <stdio.h>
+void main()
+{
+   int num, cont = 0;
+   printf("\a----- Tabla de multiplicar -----\n");
+   printf("Ingrese un número: \n");
+   scanf("%d", &num);
+   printf("La tabla de multiplicar del %d es:\n", num);
+   while (++cont <= 10)
+   printf("%d x %d = %d\n", num, cont, num*cont);
+}
